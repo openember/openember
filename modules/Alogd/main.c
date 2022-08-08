bb83cd3cda986e0a0d3d0aef836a4f4316cd6721
luhuadong <luhuadong@163.com>
2022-08-08 17:50:16 +0800

    [mod] add Alogd daemon

---
+++ modules/Alogd/main.c
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2006-2020, DreamGrow Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2022-08-08     luhuadong    the first version
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define MODULE_NAME            "alogd"
+#define LOG_TAG                MODULE_NAME
+#include "agloo.h"
+
+
+int main(void)
+{
+    int rc;
+    
+    log_init(MODULE_NAME);
+    LOG_I("Version: %lu.%lu.%lu", AG_VERSION, AG_SUBVERSION, AG_REVISION);
+
+    daemon(0, 0);
+
+    while (1) {
+        sleep(1);
+    }
+
+    return 0;
+}
